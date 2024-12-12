58e9d01c80f820f34fcb50f3a7f1ee62db5dedcf
omerkisa1 <omereis.5555@gmail.com>
2024-12-12 18:44:43 +0300

    directional light global position

--- godot4eclipse/eclipse2/directional_light_3d.gd
+++ godot4eclipse/eclipse2/directional_light_3d.gd
@@ -1,12 +1,6 @@
 extends DirectionalLight3D
 
-@export var sun_node_path: NodePath
+@export var earth_node_path: NodePath  # Dünya'nın yolunu belirtmek için
 
-func _ready():
-	if sun_node_path:
-		# Güneş düğümünü bul
-		var sun = get_node("root/Node/Node3D/sun")
-		if sun:
-			global_position = sun.global_position
-			# Işık sürekli olarak güneş ile aynı yerde kalacak
-			look_at(Vector3(0, 0, 0)) # Opsiyonel: Işığın hangi yöne bakacağı
+func _process(delta):
+	look_at(get_node("/root/Node/Node3D/EarthOrbit/Earth").global_position)
